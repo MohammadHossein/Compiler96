b3dc7c76b2c5223d9eab9a71a6b1d7dadf309391
a.jafari <alijafari2019@gmail.com>
2017-11-05 17:21:30 +0330

    fixed float number and int number some other change

--- lex.py
+++ lex.py
@@ -40,8 +40,8 @@ class Lexer:
         'DIV_EXP',
         'PLUSPLUS',
         'MINUSMINUS',
-        'CIRCUIT_OR_KW',
-        'CIRCUIT_AND_KW',
+        'THEN_OR_KW',
+        'THEN_AND_KW',
         'OR_KW',
         'AND_KW',
         'NOT_KW',
@@ -62,7 +62,8 @@ class Lexer:
         'MAIN',
         'ID',
         'COMMENT',
-        'NUMBER',
+        'NUMBER_INT',
+        'NUMBER_FLOAT'
     )
 
     sTable = []
@@ -72,6 +73,9 @@ class Lexer:
            r'|\u0645|\u0646|\u0647|\u0648|\u0649|\u06A9|\u064A|\u06CC|\u06BE|\u06D5|\u06C1|\_'
     adad = r'\u0660|\u0661|\u0662|\u0663|\u0664|\u0665|\u0666|\u0667|\u0668|\u0669|\u06F0|\u06F1|\u06F2|\u06F3' \
            r'|\u06F4|\u06F5|\u06F6|\u06F7|\u06F8|\u06F9|0|1|2|3|4|5|6|7|8|9'
+    adadNoneZero = r'\u0661|\u0662|\u0663|\u0664|\u0665|\u0666|\u0667|\u0668|\u0669|\u06F1|\u06F2|\u06F3' \
+           r'|\u06F4|\u06F5|\u06F6|\u06F7|\u06F8|\u06F9|1|2|3|4|5|6|7|8|9'
+
     semi_colon_farsi = r'\u061B'
     comma_farsi = r'\u060C'
 
@@ -97,8 +101,8 @@ class Lexer:
         'وقتی': 'WHILE_KW',
         'برگردان': 'RETURN_KW',
         'بشکن': 'BREAK_KW',
-        'یا': 'CIRCUIT_OR_KW',
-        'و': 'CIRCUIT_AND_KW',
+        'یا': 'THEN_OR_KW',
+        'و': 'THEN_AND_KW',
         'یا وگرنه': 'OR_KW',
         'و همچنین': 'AND_KW',
         'خلاف': 'NOT_KW',
@@ -136,9 +140,12 @@ class Lexer:
 
     # t_ID = r'[آ-ی|\_][آ-ی|۰-۹|0-9['
     # t_ID = r'[' + harf + r'][' + harf + r'|' + adad + r']+'
-    t_NUMBER = r'[' + adad + r']+'
+    t_NUMBER_INT = r'[' + adadNoneZero + r']' + r'[' + adad + r']*'
+    t_NUMBER_FLOAT = r'(([' + adadNoneZero + r']' + r'[' + adad + r']*)|(0|\u0660|\u06F0))' + r'\.((0|\u0660|\u06F0)|' + r'([' + adad + r']*' + r'[' + adadNoneZero + r']))'
+
+
     t_FIXED_CHARACTER = r'\\.{1}'
-    t_ignore = r' \t\r\f\v'
+    t_ignore = r' '
 
     def t_ID(self, t):
         r'[\u0622|\u0627|\u0628|\u067E|\u062A|\u062B|\u062C|\u0686|\u062D|\u062E|\u062F|\u0630|\u0631|\u0632' \
@@ -146,25 +153,26 @@ class Lexer:
         r'|\u0645|\u0646|\u0647|\u0648|\u0649|\u06A9|\u064A|\u06CC|\u06BE|\u06D5|\u06C1|\_][\u0622|\u0627|\u0628|\u067E|\u062A|\u062B|\u062C|\u0686|\u062D|\u062E|\u062F|\u0630|\u0631|\u0632' \
         r'|\u0698|\u0633|\u0634|\u0635|\u0636|\u0637|\u0638|\u0639|\u063A|\u0641|\u0642|\u0643|\u06AF|\u0644' \
         r'|\u0645|\u0646|\u0647|\u0648|\u0649|\u06A9|\u064A|\u06CC|\u06BE|\u06D5|\u06C1|\_|\u0660|\u0661|\u0662|\u0663|\u0664|\u0665|\u0666|\u0667|\u0668|\u0669|\u06F0|\u06F1|\u06F2|\u06F3' \
-        r'|\u06F4|\u06F5|\u06F6|\u06F7|\u06F8|\u06F9|0|1|2|3|4|5|6|7|8|9]+'
+        r'|\u06F4|\u06F5|\u06F6|\u06F7|\u06F8|\u06F9|0|1|2|3|4|5|6|7|8|9]*'
         t.type = self.reserved.get(t.value, 'ID')  # Check for reserved words
         if t.type == 'ID':
             if t.value not in self.sTable:
                 self.sTable.append(t.value)
 
-
         return t
 
     def t_COMMENT(self, t):
-        r'/\*([^*]|[\r\n]|(\*+([^*/]|[\r\n])))*\*+/'
+        r'/\*([^*]|[\r\n]|(\*+([^*/]|[\r\n])))*\*+/|//.*'
+        # r'//.*'
         pass
 
     def t_newline(self, t):
-        r'\n+'
+        r'\r?\n+'
         t.lexer.lineno += t.value.count("\n")
 
     def t_error(self, t):
-        # print("Illegal character '%s'" % t.value)
+        if t.value[0] != "0" and t.value[0] != '\u0660' and t.value[0] != '\u06F0':
+            print("Illegal character '%s'" % (t.value[0]))
         t.lexer.skip(1)
 
     def build(self, **kwargs):
@@ -174,6 +182,7 @@ class Lexer:
         self.lexer = lex.lex(module=self, **kwargs)
 
         return self.lexer
+
 
 if __name__ == '__main__':
     lexer = Lexer().build()
@@ -186,10 +195,13 @@ if __name__ == '__main__':
         if not tok:
             break  # No more input
         parsIndex = '-'
+        # if tok.type == 'ID':
+        #     for i in range(len(Lexer.sTable)):
+        #         if Lexer.sTable[i] == tok.value:
+        #             parsIndex = i
+        #             break
+        i=-1
         if tok.type == 'ID':
-            for i in range(len(Lexer.sTable)):
-                if Lexer.sTable[i] == tok.value:
-                    parsIndex = i
-                    break
-
+            i = Lexer.sTable.index(tok.value)
+        parsIndex = {'-1': '-'}.get(str(i), str(i))
         print(tok.value + "\t" + tok.type + "\t" + str(parsIndex))

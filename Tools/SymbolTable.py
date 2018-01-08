ee7aec6fd311a4d99b8726c93904d59264a9a1d0
MohammadHossein <mhhessar1376@gmail.com>
2018-01-08 10:18:21 +0330

    SymbolTable class

--- Tools/SymbolTable.py
+++ Tools/SymbolTable.py
@@ -1,5 +1,39 @@
-
-
 class SymbolTable:
-    sTable = None
-    
+    def __init__(self):
+        self.table = self.mktable(None)
+
+    def mktable(self, previous):
+        table = Table()
+        table.parent = previous
+        return table
+
+    def enter(self, table, name, type, offset):
+        table.items.append(TableItem(name, type, offset, table))
+
+    def addwidth(self, table, width):
+        table.header = width
+
+    def enterproc(self, table, name, newTable):
+        table.items.append(TableProc(name, newTable, table))
+
+
+class Table:
+    def __init__(self):
+        self.parent = None
+        self.header = None
+        self.items = []
+
+
+class TableItem:
+    def __init__(self, name, type, offset, table=None):
+        self.name = name
+        self.type = type
+        self.offset = offset
+        self.table = table
+
+
+class TableProc:
+    def __init__(self, name, table, parentTable):
+        self.name = name
+        self.table = table
+        self.parentTable = parentTable
